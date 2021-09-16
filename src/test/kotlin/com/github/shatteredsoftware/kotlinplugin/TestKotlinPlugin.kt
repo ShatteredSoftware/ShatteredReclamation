1c15bd87e0f4823b3010f5c564a25e686088fcfd
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-09-16 13:53:04 -0600

    :fire: Remove old imports in TestKotlinPlugin.

--- src/test/kotlin/com/github/shatteredsoftware/kotlinplugin/TestKotlinPlugin.kt
+++ src/test/kotlin/com/github/shatteredsoftware/kotlinplugin/TestKotlinPlugin.kt
@@ -1,8 +1,5 @@
 package com.github.shatteredsoftware.kotlinplugin
 
-import be.seeseemelk.mockbukkit.MockBukkit
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class TestKotlinPlugin {
